70d214dfadd83d05d693a10f3f390f540845a453
tkzn <andre.yabiku@hotmail.com>
2025-09-14 17:04:47 -0300

    adding cpf validation

--- src/main/java/com/addresses/core/usecases/impl/UseCaseImpl.java
+++ src/main/java/com/addresses/core/usecases/impl/UseCaseImpl.java
@@ -1,15 +1,18 @@
 package com.addresses.core.usecases.impl;
 
+import com.addresses.core.usecases.UseCase;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 @Component
 @Data
 @Builder
-public class UseCaseImpl {
+public class UseCaseImpl implements UseCase {
 
-    Boolean verify(String cpf) {
-        return true;
+    @Override
+    public Mono<Boolean> verify(String cpf) {
+        return Mono.just(cpf.contains("00"));
     }
 }
