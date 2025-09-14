baff9c41fcdf4c539089ee67c5cb481bc996fafa
tkzn <andre.yabiku@hotmail.com>
2025-09-14 13:20:44 -0300

    creating service

---
+++ src/main/java/com/addresses/application/impl/ServiceOrchestratorImpl.java
@@ -0,0 +1,37 @@
+package com.addresses.application.impl;
+
+import com.addresses.application.ServiceOrchestrator;
+import com.addresses.core.model.Address;
+import com.addresses.core.usecases.UseCase;
+import com.addresses.external.OutsideAPI;
+import com.addresses.external.Request;
+import com.addresses.external.SensitiveDataAPI;
+import com.addresses.external.persistance.Repository;
+import com.addresses.external.persistance.dto.User;
+import lombok.Builder;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+@Service
+@Builder
+public class ServiceOrchestratorImpl implements ServiceOrchestrator {
+    private final Repository repository;
+    private final SensitiveDataAPI sensitiveDataAPI;
+    private final OutsideAPI outsideAPI;
+    private final UseCase useCase;
+
+    @Override
+    public Mono<Address> execute(Request request) {
+        User user = User.builder().cpf(request.getCpf()).email(request.getEmail()).build();
+        return repository.save(user)
+                .subscribeOn(Schedulers.boundedElastic())
+                .flatMap(it -> outsideAPI.getAddress(request.getZipCode()))
+                .flatMap(it -> useCase.verify(request.getCpf()))
+                .filter(bool -> bool)
+                .flatMap(it -> sensitiveDataAPI.getSensitiveData(request.getCpf()))
+                .flatMap(it -> Mono.just(new Address()))
+                .switchIfEmpty(Mono.just(new Address()));
+    }
+}
