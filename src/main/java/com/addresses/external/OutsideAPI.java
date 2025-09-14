0b0c07b14c1cd02091f2263af82501a22868f834
tkzn <andre.yabiku@hotmail.com>
2025-09-14 17:04:27 -0300

    retrieving address info

--- src/main/java/com/addresses/external/OutsideAPI.java
+++ src/main/java/com/addresses/external/OutsideAPI.java
@@ -1,17 +1,21 @@
 package com.addresses.external;
 
 import com.addresses.core.model.Address;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+@Component
 public class OutsideAPI {
 
+    private static final Logger log = LoggerFactory.getLogger(OutsideAPI.class);
     WebClient webClient = WebClient.create();
 
     public Mono<Address> getAddress(String zipCode) {
         return webClient.get()
                 .uri("https://brasilapi.com.br/api/cep/v1/" + zipCode)
-                .retrieve()
-                .bodyToMono(Address.class);
+                .exchangeToMono(clientResponse -> clientResponse.bodyToMono(Address.class));
     }
 }
